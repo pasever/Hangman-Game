13ddbe01863f47212c1849075f9d4bffa285dbf1
Pasever <pas1034ever@gmail.com>
2017-09-13 00:15:05 -0400

    a.1.0

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,154 @@
 var moviesArray = ["The Godfather", "The Shawshank Redemption", "Schindler's List", "Raging Bull", "Casablanca", "Citizen Kane", "Gone with the Wind", 
-				   "The Wizard of Oz", "One Flew Over the Cuckoo's Nest", "Lawrence of Arabia", "Vertigo", "Psycho", "The Godfather: Part II", "On the Waterfront", 
+				   "The Wizard of Oz", "One Flew Over the Cuckoo's Nest", "Lawrence of Arabia", "Vertigo", "Psycho", "The Godfather Part II", "On the Waterfront", 
 				   "Sunset Blvd", "Forrest Gump", "The Sound of Music", "Angry Men", "West Side Story", "Star Wars: A New Hope", "A Space Odyssey", 
-				   "E.T. the Extra-Terrestrial", "The Silence of the Lambs", "Chinatown", "The Bridge on the River Kwai", "Singing in the Rain", 
-				   "It is a Wonderful Life", "Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb", "Some Like It Hot", "Ben-Hur", 
+				   "E T the Extra-Terrestrial", "The Silence of the Lambs", "Chinatown", "The Bridge on the River Kwai", "Singing in the Rain", 
+				   "It is a Wonderful Life", "Dr Strangelove or How I Learned to Stop Worrying and Love the Bomb", "Some Like It Hot", "Ben Hur", 
 				   "Apocalypse Now", "Amadeus", "The Lord of the Rings: The Return of the King", "Gladiator", "Titanic", "From Here to Eternity", 
 				   "Saving Private Ryan", "Unforgiven", "Raiders of the Lost Ark", "Rocky", "A Streetcar Named Desire", "The Philadelphia Story", 
 				   "To Kill a Mockingbird", "An American in Paris", "The Best Years of Our Lives", "My Fair Lady", "A Clockwork Orange", 
 				   "Doctor Zhivago", "The Searchers", "Jaws"]
 
 
-
-
+var wordInProgress = [];
+var remainingGuesses = 10;
+var winsCounter = 0;
+var lettersGuessed = "";
 var randomWord = moviesArray[Math.floor(Math.random() * moviesArray.length)];
 var randomWordArray = [];
+randomWord = randomWord.toUpperCase();
 
+
+
+// creates the number of button = to lenght of the word
+// gets rid of all the spaces
 function generate() {
 
 	for (i=0; i < randomWord.length; i++) {
+		
+		if (randomWord[i] === ' ') {
 
-		var button = document.createElement("button");
-	    button.innerHTML = "";
-	    if (randomWord[i] != "") {}
-	    document.getElementById('blocks').appendChild(button);
+			wordInProgress[wordInProgress.length - 1].className = "gap";
+
+		} else {
+
+			console.log(randomWord[i], randomWord[i] !== ' ');
+
+			var button = document.createElement("button");
+
+			wordInProgress.push(button);
+		    
+		    document.getElementById('blocks').appendChild(button);
+
+		}
+		
 	}
 	
 }
-    
-
+   
+//starting button that/hides after click
 $("#startingButton").on("click", function() {
-	generate();
+	
 	$("#startingButton").remove();
+	generate();
 
 });
 
+// checked if the pressed button is correct 
+function correctGuess(letter) {
+
+	var cur = 0;
+
+	// Replace all letters that were guessed correctly with the correct letter
+	for (var i = 0; i < randomWord.length; i++) {
+
+		if (randomWord.charAt(i) === ' ') continue;
+
+		if (randomWord.charAt(i) === letter) {
+			wordInProgress[cur].innerHTML = letter;
+			//document.getElementById("finalWord").innerHtml = wordInProgress.join('');
+		}
+
+		cur++;
+
+	}
+		
+}			
+			
+// accepts the key press and saves the guess into a value
+document.onkeyup = function(event){
+
+	var guess = String.fromCharCode(event.keyCode).toUpperCase();
+
+	console.log(guess);
+
+	correctGuess(guess);
+}	
+
+// Checks if the word is complete
+if (wordInProgress == randomWord) {
+			winsCounter++;
+	//somehow have to restart the game or add a reset button 		
+		}
 
 
 
 
 
+
+
+
+// function (incorrectGuess) {
+// 		// Ignore if the guess has already been made
+// 		if (lettersGuessed.indexOf(guess) == -1) {
+// 			var letterSpan;
+
+// 			// Add guess to lettersGuessed variable
+// 			lettersGuessed += guess;
+
+// 			// If lettersGuessed has 1 or more guesses, add trailing space
+// 			// Else remove existing space
+// 			if (lettersGuessed.length > 1){
+// 				var spaceSpan = document.createElement("span");
+// 				spaceSpan.innerHTML = "&nbsp;&nbsp;";
+// 				document.getElementById("lettersGuessed").appendChild(spaceSpan);
+// 			} else {
+// 				document.getElementById("lettersGuessed").innerHTML = "";
+// 			}
+
+// 		}
+		
+
+// function(pushChanges) {
+// 		// Update all text fields
+// 		document.getElementById("winsCounter").innerHTML = winsCounter;
+// 		document.getElementById("wordInProgress").innerHTML = wordInProgress;
+// 		document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+// }
+
+
+
+
+
+
+// // check if pressed key is a letter 
+// /*
+// document.onkeyup = function letterChecker () {
+// 	if ()
+
+
+// } */
+
+// function(checkGuess) {
+// 	if (/a-zA-Z/.test(guess)) {
+// 		if (wordObject.word.indexOf(guess) > -1 {
+// 			correctGuess(guess);
+// 		} else {
+// 			wrongGuess(guess);
+// 		}
+// 	}
+// }
+
+
+
+
+// */
---
+++ assets/javascript/keyboard.js
@@ -0,0 +1,22 @@
+
+/*
+window.onload = function alphabetButtons() {
+    document.getElementById('buttons');
+    buttons.innerHTML = "";
+    var alphaButtons = document.getElementById("buttons");
+    var letters = document.createElement('ul');
+    var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+        't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    for (var i = 0; i < alphabet.length; i++) {
+      letters.id = 'alphabet';
+      var list = document.createElement('li');
+      list.id = 'letter';
+      list.innerHTML = alphabet[i];
+      list.onclick = generate;
+      alphaButtons.appendChild(letters);
+      letters.appendChild(list); 
+  }
+}
+*/
