798efb6711f2033004496a5882a8457b57fde4f2
Pasever <pas1034ever@gmail.com>
2017-09-25 22:44:41 -0400

    a.2.1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 var moviesArray = ["The Godfather", "The Shawshank Redemption",  "Schindler's List", "Raging Bull", "Casablanca", "Citizen Kane", "Gone with the Wind",
-	"The Wizard of Oz", "One Flew Over the Cuckoo's Nest", "Lawrence of Arabia", "Vertigo", "Psycho", "The Godfather Part II", "On the Waterfront",
-	"Sunset Blvd", "Forrest Gump", "The Sound of Music", "Angry Men", "West Side Story", "Star Wars A New Hope", "A Space Odyssey",
-	"E T the Extra Terrestrial", "The Silence of the Lambs", "Chinatown", "The Bridge on the River Kwai", "Singing in the Rain",
-	"It is a Wonderful Life", "Dr Strangelove or How I Learned to Stop Worrying and Love the Bomb", "Some Like It Hot", "Ben Hur",
-	"Apocalypse Now", "Amadeus", "The Lord of the Rings The Return of the King", "Gladiator", "Titanic", "From Here to Eternity",
-	"Saving Private Ryan", "Unforgiven", "Raiders of the Lost Ark", "Rocky", "A Streetcar Named Desire", "The Philadelphia Story",
-	"To Kill a Mockingbird", "An American in Paris", "The Best Years of Our Lives", "My Fair Lady", "A Clockwork Orange",
-	"Doctor Zhivago", "The Searchers", "Jaws"
+    "The Wizard of Oz", "One Flew Over the Cuckoo's Nest", "Lawrence of Arabia", "Vertigo", "Psycho", "The Godfather Part II", "On the Waterfront",
+    "Sunset Blvd", "Forrest Gump", "The Sound of Music", "Angry Men", "West Side Story", "Star Wars A New Hope", "A Space Odyssey",
+    "E T the Extra Terrestrial", "The Silence of the Lambs", "Chinatown", "The Bridge on the River Kwai", "Singing in the Rain",
+    "It is a Wonderful Life", "Dr Strangelove or How I Learned to Stop Worrying and Love the Bomb", "Some Like It Hot", "Ben Hur",
+    "Apocalypse Now", "Amadeus", "The Lord of the Rings The Return of the King", "Gladiator", "Titanic", "From Here to Eternity",
+    "Saving Private Ryan", "Unforgiven", "Raiders of the Lost Ark", "Rocky", "A Streetcar Named Desire", "The Philadelphia Story",
+    "To Kill a Mockingbird", "An American in Paris", "The Best Years of Our Lives", "My Fair Lady", "A Clockwork Orange",
+    "Doctor Zhivago", "The Searchers", "Jaws"
 ];
 
 
@@ -28,39 +28,39 @@ var wrongBool = false;
 // gets rid of all the spaces
 function generate() {
 
-	for (i = 0; i < randomWord.length; i++) {
+    for (i = 0; i < randomWord.length; i++) {
 
-		if (randomWord[i] === ' ') {
+        if (randomWord[i] === ' ') {
 
-			wordInProgress[wordInProgress.length - 1].className = "gap";
+            wordInProgress[wordInProgress.length - 1].className = "gap";
 
-		} else {
+        } else {
 
-			console.log(randomWord[i], randomWord[i] !== ' ');
-			var button = document.createElement("button");
-			wordInProgress.push(button);
-			document.getElementById('blocks').appendChild(button);
-		}
+            console.log(randomWord[i], randomWord[i] !== ' ');
+            var button = document.createElement("button");
+            wordInProgress.push(button);
+            document.getElementById('blocks').appendChild(button);
+        }
 
-	}
+    }
 
 }
 
 //starting button that hides after click
 $("#startingButton").on("click", function () {
 
-	$("#startingButton").hide();
-	generate();
+    $("#startingButton").hide();
+    generate();
 
 });
 
 // accepts the key press and saves the guess into a value
 document.onkeyup = function (event) {
 
-	var guess = String.fromCharCode(event.keyCode).toUpperCase();
+    var guess = String.fromCharCode(event.keyCode).toUpperCase();
 
-	console.log(guess);
-	correctGuess(guess);
+    console.log(guess);
+    correctGuess(guess);
 
 };
 
@@ -69,61 +69,77 @@ document.onkeyup = function (event) {
 // checked if the pressed button is correct 
 function correctGuess(letter) {
 
-	var cur = 0;
+    var cur = 0;
 
-	// Replace all letters that were guessed correctly with the correct letter
-	for (var i = 0; i < randomWord.length; i++) {
+    // Replace all letters that were guessed correctly with the correct letter
+    for (var i = 0; i < randomWord.length; i++) {
 
-		if (randomWord[i] === ' ') continue;
-		if (randomWord[i] === letter) {
+        if (randomWord[i] === ' ')
+            continue;
+        if (randomWord[i] === letter) {
 
-			wordInProgress[cur].innerHTML = letter;
-			finalWord[cur] = letter;
-			checkWin();
+            wordInProgress[cur].innerHTML = letter;
+            finalWord[cur] = letter;
+            checkWin();
 
 
 
-			// for loop that checks if letter is at index of lettersGuessedArray, if not 
-			for (var j = 0; j < lettersGuessedArray.length; j++) {
+            // for loop that checks if letter is at index of lettersGuessedArray, if not 
+            for (var j = 0; j < lettersGuessedArray.length; j++) {
 
-				if (lettersGuessedArray.indexOf(letter) == -1) { // -1 from indexOf means "not found"
+                if (lettersGuessedArray.indexOf(letter) == -1) { // -1 from indexOf means "not found"
 
-					lettersGuessed = lettersGuessed + 1;
-					lettersGuessedArray.push(letter);
-					$('#lettersGuessed').html(lettersGuessed);
-					wrongBool = true;
-				}
-			}
+                    lettersGuessed = lettersGuessed + 1;
+                    lettersGuessedArray.push(letter);
+                    $('#lettersGuessed').html(lettersGuessed);
+                    wrongBool = true;
+                }
+            }
 
-			document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
-		}
+            document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+        }
 
-		cur++;
-	}
+        cur++;
+    }
 
-	//for loop ends
-	if (wrongBool === false) {
+    //for loop ends
+    if (wrongBool === false) {
 
-		remainingGuesses = remainingGuesses - 1;
-		document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
-	}
-	
-	wrongBool = false;
+        remainingGuesses = remainingGuesses - 1;
+        document.getElementById("remainingGuesses").innerHTML = remainingGuesses;
+    }
+
+    wrongBool = false;
 
 }
 
 // Checks if the word is complete
-function checkWin () {
-	console.log('final word: ', finalWord.join(''));
-        const word = randomWord.replace(/\s+/g, "")
-        	console.log('random word: ', word);
+function checkWin() {
+    console.log('final word: ', finalWord.join(''));
+    const word = randomWord.replace(/\s+/g, "")
+    console.log('random word: ', word);
 
-	if ( finalWord.join('') == word) {
-		alert("Congratulations! You won!");
-		winsCounter++;
-		console.log(winsCounter);
-		//somehow have to restart the game or add a reset button 		
-	}
+    if (finalWord.join('') == word) {
+
+        winsCounter++;
+        $("#winsCounter").html(winsCounter);
+        alert("Congratulations! You won!");
+        reset();
+
+
+        //somehow have to restart the game or add a reset button 		
+    }
+}
+
+function reset() {
+
+//  $("#startingButton").show(); 
+    document.getElementById('blocks').removeChild();
+
+
+
+
+
 }
 
 
